851918efd46753b108506b77704c114782c6ea83
Ruben Cardenas <rcardenas@tacitknowledge.com>
2020-07-25 11:53:53 -0500

    Adding admin links to the nav bars

---
+++ admin/includes/left-sidebar.php
@@ -0,0 +1,38 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li>
+            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        </li>
+        <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#posts-dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="posts-dropdown" class="collapse">
+                <li>
+                    <a href="#">Add posts</a>
+                </li>
+                <li>
+                    <a href="#">View all posts</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="tables.html"><i class="fa fa-fw fa-table"></i> Categories</a>
+        </li>
+        <li>
+            <a href="forms.html"><i class="fa fa-fw fa-edit"></i> Comments</a>
+        </li>
+        <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#users"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="users" class="collapse">
+                <li>
+                    <a href="#">Dropdown Item</a>
+                </li>
+                <li>
+                    <a href="#">Dropdown Item</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="forms.html"><i class="fa fa-fw fa-edit"></i> Profile</a>
+        </li>
+    </ul>
+</div>

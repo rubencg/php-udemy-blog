7c38ecff76aaa247b2d26cb833b7f19c44799823
Ruben Cardenas <rcardenas@tacitknowledge.com>
2020-07-25 13:27:01 -0500

    Added update category. Closes #2

--- admin/categories.php
+++ admin/categories.php
@@ -18,27 +18,61 @@
                     </h1>
 
                     <div class="col-xs-6">
-                        <form action="" method="post">
-                            <?php
-                            // Add category
-                            if (isset($_POST['title']) && !empty($_POST['title'])) {
-                                $category = R::dispense('categories');
-                                $category->title = $_POST['title'];
-                                R::store($category);
-                            }
-                            // Delete category
-                            if (isset($_GET['delete'])) {
-                                $category = R::load( 'categories', $_GET['delete'] );
-                                R::trash( $category );
-                                header("Location: categories.php");
-                            }
+                        <?php
+                        // Delete category
+                        if (isset($_GET['delete'])) {
+                            $category = R::load('categories', $_GET['delete']);
+                            R::trash($category);
+                            header("Location: categories.php");
+                        }
+                        ?>
+                        <div class="row">
+                            <form action="" method="post">
+                                <?php
+                                // Add category
+                                if (isset($_POST['title']) && !empty($_POST['title'])) {
+                                    $category = R::dispense('categories');
+                                    $category->title = $_POST['title'];
+                                    R::store($category);
+                                }
+                                ?>
+                                <div class="form-group">
+                                    <label for="title">Add Category</label>
+                                    <input class="form-control" type="text" name="title">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Add category</button>
+                            </form>
+                        </div>
+                        <br>
+                        <?php
+                        // Edit
+                        if (isset($_POST['edit_title']) && isset($_GET['edit'])) {
+                            $category = R::load('categories', $_GET['edit']);
+                            $category->title = $_POST['edit_title'];
+                            R::store( $category );
+                            header("Location: categories.php");
+                        }
+
+                        if (isset($_GET['edit'])) {
+                            $category = R::load('categories', $_GET['edit']);
                             ?>
-                            <div class="form-group">
-                                <label for="title">Add Category</label>
-                                <input class="form-control" type="text" name="title">
+                            <div class="row">
+                                <form action="" method="post">
+                                    <?php
+
+                                    ?>
+                                    <div class="form-group">
+                                        <label for="title">Edit Category</label>
+                                        <input autofocus="autofocus" class="form-control" type="text" name="edit_title" value="<?php echo $category->title; ?>">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Update category</button>
+                                </form>
                             </div>
-                            <button type="submit" class="btn btn-primary">Add category</button>
-                        </form>
+                            <?php
+
+                        }
+                        ?>
+
                     </div>
                     <div class="col-xs-6">
                         <table class="table">
@@ -46,6 +80,7 @@
                             <tr>
                                 <th scope="col">Id</th>
                                 <th scope="col">Category Title</th>
+                                <th></th>
                                 <th></th>
                             </tr>
                             </thead>
@@ -59,6 +94,7 @@
                                     <td><?php echo $category['id'] ?></td>
                                     <td><?php echo $category['title'] ?></td>
                                     <td><a href="categories.php?delete=<?php echo $category['id'] ?>">Delete</a></td>
+                                    <td><a href="categories.php?edit=<?php echo $category['id'] ?>">Edit</a></td>
                                 </tr>
                                 <?php
                             }

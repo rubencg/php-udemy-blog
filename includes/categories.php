cf09939fd14f0883f9b3d5304deb5c21388f499e
Ruben Cardenas <rcardenas@tacitknowledge.com>
2020-07-25 11:18:06 -0500

    Added categories to the sidebar

---
+++ includes/categories.php
@@ -0,0 +1,23 @@
+
+
+<div class="well">
+    <h4>Blog Categories</h4>
+    <div class="row">
+        <div class="col-lg-12">
+            <ul class="list-unstyled">
+                <?php
+
+                foreach ($categories as $category) {
+
+                    echo "<li><a href=\"#\">{$category['cat_title']}</a></li>";
+
+                }
+
+                ?>
+
+            </ul>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+</div>

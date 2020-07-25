5569e35f38d2080d7353cb1d7025e29da53dbf31
Ruben Cardenas <rcardenas@rubens-mbp.lan>
2020-07-25 09:09:14 -0500

    Init

---
+++ includes/db.php
@@ -0,0 +1,24 @@
+<?php
+
+require 'rb-mysql.php';
+
+R::setup('mysql:host=localhost;dbname=cms',
+    'root', '');
+
+//$db['db_host'] = 'localhost';
+//$db['db_username'] = 'root';
+//$db['db_password'] = '';
+//$db['db_database'] = 'cms';
+//
+//foreach ($db as $key => $value) {
+//    define(strtoupper($key), $value);
+//}
+//
+//$connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+//
+//if($connection){
+//    echo 'We are connected';
+//}
+
+
+?>
